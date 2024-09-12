23a3889d39fc49b3ebc1728ea32d8cf7e96041d0
Tautvydas Bagdžius <s057522@ad.viko.lt>
2024-09-12 11:46:04 +0300

    Klases darbas

--- 1Darbas/main.cpp
+++ 1Darbas/main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+// 1 uzduotis
+
+string Vardas = "Tautvydas";
+string Pavarde = "Bagdzius";
+int Amzius = 24;
+int Grupe = 1;
+int Kursas = 1;
+string Programa = "Sistemu programos";
+
+// 2 uzduotis
+
+string Pavadinimas2 = "Kauno Zalgiris";
+int Metai2 = 1995;
+string Savininkas = "Anonimas Anonimu";
+string Arena = "Zalgirio";
+int VietuSk = 5000;
+
+// 3 uzduotis
+
+string Marke = "Subaru";
+string Modelis = "Forrester";
+int Metai3 = 2016;
+double Litrazas = 5.6;
+string Spalva = "Balta";
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // 1 uzduotis
+
+    cout << Vardas; cout << " ";
+    cout << Pavarde; cout << " ";
+    cout << Amzius; cout << " ";
+    cout << Grupe; cout << "Grupe ";
+    cout << Kursas; cout << "Kursas " << endl;
+
+
+    // 2 uzduotis
+
+    cout << Pavadinimas2; cout << " ";
+    cout << Metai2; cout << " ";
+    cout << Savininkas; cout << " ";
+    cout << Arena; cout << " stadijonas ";
+    cout << VietuSk; cout << "vietu" << endl;
+
+    // 3 uzduotis
+
+
     return 0;
 }
