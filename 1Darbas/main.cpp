ce6e7209386c8618963110ee25782aca3a42f562
Tautvydas Bagdžius <s057522@ad.viko.lt>
2024-09-13 09:58:50 +0300

    2024-09-13 paskaitos darbas

--- 1Darbas/main.cpp
+++ 1Darbas/main.cpp
@@ -28,6 +28,24 @@ int Metai3 = 2016;
 double Litrazas = 5.6;
 string Spalva = "Balta";
 
+// 4 uzduotis
+
+int a = 13;
+int b = 5;
+double c = 17.5;
+
+// 5 uzduotis
+
+double aa = 0;
+double bb = 0;
+double cc = 0;
+double dd = 0;
+double ee = 0;
+
+// 6 uzduotis
+
+int aaa = 0;
+
 int main()
 {
     // 1 uzduotis
@@ -35,8 +53,8 @@ int main()
     cout << Vardas; cout << " ";
     cout << Pavarde; cout << " ";
     cout << Amzius; cout << " ";
-    cout << Grupe; cout << "Grupe ";
-    cout << Kursas; cout << "Kursas " << endl;
+    cout << Grupe; cout << " Grupe ";
+    cout << Kursas; cout << " Kursas " << endl << endl;
 
 
     // 2 uzduotis
@@ -45,9 +63,42 @@ int main()
     cout << Metai2; cout << " ";
     cout << Savininkas; cout << " ";
     cout << Arena; cout << " stadijonas ";
-    cout << VietuSk; cout << "vietu" << endl;
+    cout << VietuSk; cout << "vietu" << endl << endl;
 
     // 3 uzduotis
+
+    cout << Marke; cout << " ";
+    cout << Modelis; cout << " ";
+    cout << Metai3; cout << " ";
+    cout << Litrazas; cout << " ";
+    cout << Spalva << endl << endl;
+
+    // 4 uzduotis
+
+    cout << a+b-c << endl;
+    cout << 15/2+c << endl;
+    cout << a/static_cast<double>(b)+2*c << endl;
+    cout << 14%3+6.3+b/a << endl;
+    cout << static_cast<int>(c)%5+a-b << endl;
+    cout << 13.5/2+4.0*3.5+18 << endl << endl;
+
+    // 5 uzduotis
+
+    cout << "Ivesk 5 skaicius" << endl;
+    cin >> aa; cin >> bb; cin >> cc; cin >> dd; cin >> ee;
+    double ff = (aa+bb+cc+dd+ee)/5;
+    cout << ff << endl << endl;
+
+    // 6 uzduotis
+
+    cout << "Ivesk dvzenkli skaiciu" << endl;
+    cin >> aaa;
+    int bbb = aaa%10;
+    int ccc = (aaa-bbb)/10;
+    int ddd = ccc+bbb;
+    // Sudedamuju daliu skaiciavimai
+    //cout << aaa; cout << "\t"; cout << bbb; cout << "\t"; cout << ccc << endl;
+    cout << ddd << endl;
 
 
     return 0;
